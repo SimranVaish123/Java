9d3239f8db9eeafb2188f67b67dbdac0461167fc
VAISH Simran <simran.vaish@soprasteria.com>
2022-10-03 12:25:20 +0530

    Initial commit

---
+++ JunitMockitoSpringBoot/src/main/java/com/javatrainingschool/service/BookService.java
@@ -0,0 +1,27 @@
+package com.javatrainingschool.service;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javatrainingschool.entity.Book;
+import com.javatrainingschool.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	BookRepository bookRepo;
+	
+	public List<Book> getAllBooks() {
+		List<Book> allBooks = (List<Book>) bookRepo.findAll();
+		List<Book> author = (List<Book>) bookRepo.findAll();
+		
+		allBooks.forEach(b -> b.setName(b.getName()+"-b"));
+		author.forEach(c -> c.setAuthor(c.getAuthor()+"-b"));
+		return allBooks;
+	}
+}
